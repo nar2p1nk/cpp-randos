e834e5945d2b60ed049cdf1a64ffe40ec62ba3b8
nar2p1nk <farrisisqandar@gmail.com>
2022-03-27 23:40:04 +0800

    INIT

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name = "floyd";
+
+    cout << name << endl;
+    cout << "Hatred!" << endl;
+    return 0;
+}
---
+++ yourName.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    cout << "what is your name?: ";
+
+    cin >> name;
+
+    cout << "your name is " << name;
+
+    return 0;
+}
