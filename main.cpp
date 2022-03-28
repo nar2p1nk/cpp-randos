04651c6d3c0ddc0d069eaa35c2e37a6a6cba6ff7
nar2p1nk <farrisisqandar@gmail.com>
2022-03-28 23:11:23 +0800

    this is probably the my last C++ commit, im not sure im gonna use this language in the future. sorry C++

--- main.cpp
+++ main.cpp
@@ -3,9 +3,22 @@
 using namespace std;
 
 int main(){
-    string name = "floyd";
 
-    cout << name << endl;
-    cout << "Hatred!" << endl;
+    int enum1,enum2,enum3;
+
+    int num = 1, num2 = 2, num3 = 3;
+
+    enum1 = enum2 = enum3 = num3;
+
+    int eTotal = enum1 + enum2 + enum3;
+
+    int total = num + num2 + num3;
+
+    cout << "total:" << total << endl;
+    cout << "empty total:" << eTotal << endl;
+
+//    string varia = "floyd";
+//    cout << varia << endl;
+//    cout << "Hatred!" << endl;
     return 0;
 }
---
+++ plusPlus.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1,num2;
+
+    cout << "enter a number: "; cin >> num1;
+    cout << "enter a second number: "; cin >> num2;
+
+    int total = num1 + num2;
+
+    cout << "the total is " << total;
+}
